84b41e9e5d8cf26556a89a6365818e334ee0b5a9
Emily Ritter <ritter@gcgmarketing.com>
2020-12-22 12:04:38 -0600

    Changes exmaple url and adds 'waituntil' setting so that puppeteer waits for no new network connections for at least 500 ms

--- generatePDF.js
+++ generatePDF.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 900, deviceScaleFacotr: 2 });
-  await page.goto("https://example.com");
+  await page.goto("https://observablehq.com/@kerryrodden/sequences-sunburst", {waitUntil: 'networkidle0'});
   await page.pdf({
     path: "./test.pdf",
     format: "A4",
