cb97de700bfdf395c8ec49bbdaf930096103cd60
Emily Ritter <ritter@gcgmarketing.com>
2020-12-22 10:36:13 -0600

    Changes example website

--- generatePDF.js
+++ generatePDF.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 900, deviceScaleFacotr: 2 });
-  await page.goto("https://intalytics.com");
+  await page.goto("https://example.com");
   await page.pdf({
     path: "./test.pdf",
     format: "A4",
